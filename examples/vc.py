41f75fcb87b13170738528f44c831e526fec859d
Eduardo Azevedo <eduaze@gmail.com>
2025-08-28 21:43:31 -0400

    working on real examples

--- examples/vc.py
+++ examples/vc.py
@@ -5,34 +5,19 @@ from moralhazard import MoralHazardProblem
 from moralhazard.config_maker import make_utility_cfg, make_distribution_cfg
 
 # Basics
-base_company_value = 100000 # 100bn for a public company
-ln_mu = 0 # y is a performance index 1 = median performance with perfect effort.
-ln_sigma = 0.1 # Start with public company, small variance.
-w0 = 10 # 10m consumption that a big shot CEO would have had in 10 years.
+S = 1e1
+ln_sigma = 2.3
+w0 = 1
+agent_max_impact = np.log(10) # At most multiplies the firm value by this
 
-revenue_function = lambda a: base_company_value * np.exp(a + ln_sigma**2/2)
+expected_firm_value = lambda a: S * np.exp(ln_sigma**2/2) * np.exp(agent_max_impact * a)
+expected_revenue = lambda a: expected_firm_value(a) - expected_firm_value(0)
+median_firm_value = lambda a: S * np.exp(agent_max_impact * a)
 
 
 # Cost function
-# We consider a function that would require infinite effort to achieve the target value.
-# But elasticity of the gap with respect to marginal cost of effort is something reasonable.
-alpha = 1 # Elasticity of the gap is 1 / alpha + 1, need alpha > 0
-
-def c(x):
-    """
-    Power-barrier cost: c(x) = (1 - x)^(-alpha) - 1
-    """
-    x = np.asarray(x, dtype=float)
-    with np.errstate(divide='ignore', invalid='ignore'):
-        return (1.0 - x)**(-alpha) - 1.0
-
-def mc(x):
-    """
-    Marginal cost: mc(x) = c'(x) = alpha * (1 - x)^(-alpha - 1)
-    """
-    x = np.asarray(x, dtype=float)
-    with np.errstate(divide='ignore', invalid='ignore'):
-        return alpha * (1.0 - x)**(-alpha - 1.0)
+c = lambda a: - np.log(1 - a)
+mc = lambda a: 1 / (1 - a)
 
 # Create cfgs
 utility_cfg = make_utility_cfg("log", w0=w0)
@@ -41,7 +26,7 @@ dist_cfg = make_distribution_cfg("gaussian", sigma=ln_sigma)
 
 # Reservation utility
 u = utility_cfg["u"]
-reservation_utility = u(2 * w0)
+reservation_utility = u(w0)
 
 cfg = {
     "problem_params": {
@@ -53,48 +38,62 @@ cfg = {
     "computational_params": {
         "distribution_type": "continuous",
         "y_min": - 4 * ln_sigma,
-        "y_max": 1 + 4 * ln_sigma,
+        "y_max": agent_max_impact + 4 * ln_sigma,
         "n": 201,  # must be odd
     }
 }
 
 mhp = MoralHazardProblem(cfg)
 
-result_cmp = mhp.solve_cost_minimization_problem(
-    intended_action=0.35,
-    reservation_utility=reservation_utility,
-    solver="iterative",
-    n_a_iterations=1,
-    a_ic_lb=0.0,
-    a_ic_ub=0.35,
-    a_ic_initial=0.0,
-)
-
 result_principal = mhp.solve_principal_problem(
-    revenue_function=revenue_function,
+    revenue_function=expected_revenue,
     reservation_utility=reservation_utility,
-    solver="iterative",
+    solver="a_hat",
+    a_hat=[0.0],
     a_min=0.0,
-    a_max=0.35,
-    a_init=0.0,
-    a_ic_lb=0.0,
-    a_ic_ub=0.35,
-    a_ic_initial=0.0
+    a_max=0.7,
+    a_init=0.0
 )
 
-print(result_cmp)
 print(result_principal)
 
 print("Optimal action: ", result_principal.optimal_action)
 print("Expected wage: ",result_principal.constraints["Ewage"])
 print("Optimal profits: ",result_principal.profit)
 
+a0 = result_principal.optimal_action
 v = result_principal.optimal_contract
 w = mhp.k(v)
 density = mhp._primitives["f"](mhp.y_grid, result_principal.optimal_action)
 
-# Plot results
-plt.plot(mhp.y_grid, w)
+id_mid = abs(mhp.y_grid - agent_max_impact * a0) < 3 * ln_sigma
+y_mid = mhp.y_grid[id_mid]
+firm_val = S * np.exp(y_mid)
+
+plt.plot(y_mid, w[id_mid])
+plt.title("Wage vs y")
 plt.show()
-plt.plot(mhp.y_grid, density)
+
+plt.plot(y_mid, S * np.exp(y_mid))
+plt.title("Firm value vs y")
+plt.show()
+
+plt.plot(y_mid, density[id_mid])
+plt.title("Density vs y")
+plt.show()
+
+
+
+plt.plot(firm_val, w[id_mid])
+plt.title("Wage vs firm value")
+plt.show()
+
+plt.plot(firm_val, w[id_mid] / firm_val)
+plt.xscale('log')
+plt.title("Founder share vs firm value")
+plt.show()
+
+a_grid = np.linspace(0, 1, 100)
+plt.plot(a_grid, mhp.U(v, a_grid))
+plt.title("Utility vs action")
 plt.show()
